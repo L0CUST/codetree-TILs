7404cf6ee1040d59a8df1f74bccf1d19903221d7
Son <84182829+L0CUST@users.noreply.github.com>
2024-07-03 16:07:37 +0900

    Title: 두 사람, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/두 사람/two-person.py
+++ 240703/두 사람/two-person.py
@@ -1,7 +1,9 @@
 a1, g1 = input().split()
 a2, g2 = input().split()
+a1 = int(a1)
+a2 = int(a2)
 
-if ((a1 >= "19" and g1 == "M") or (a2 >= "19" and g2 == "M")):
+if ((a1 >= 19 and g1 == "M") or (a2 >= 19 and g2 == "M")):
     print(1)
 else:
     print(0)
